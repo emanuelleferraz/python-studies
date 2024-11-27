b7e019168b291542c20314d980ee7b3cb65ec45b
Emanuelle Ferraz <manu.ferraz16@gmail.com>
2024-11-27 10:20:28 -0300

    upload de arquivos de fundamentos da linguagem

---
+++ 02 - fundamentos de python/operadores-aritmeticos.py
@@ -0,0 +1,28 @@
+x = y = z = 0
+
+x = int(input('Digite um número: '))
+y = int(input('Digite outro número: '))
+
+# Soma
+z = x + y
+print('A soma dos dois valores é', z)
+
+# Subtração
+z = x - y
+print('A diferença dos dois valores é', z)
+
+# Multiplicação
+z = x * y
+print('O produto dos dois valores é', z)
+
+# Divisão
+z = x / y
+print('A razão entre os dois valores é', z)
+
+# Módulo
+z = x % y
+print('O resto da divisão inteira entre os dois valores é', z)
+
+# Exponenciação
+z = x ** y
+print('O primeiro valor elevado ao segundo é', z)
---
+++ 02 - fundamentos de python/operadores-logicos.py
@@ -0,0 +1,20 @@
+# idade = 15
+# altura = 1.75
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+# msg = 'Pode participar do evento? ' + str(resultado)
+
+# print(msg)
+
+# Programa de disparo de alarme
+# porta = 'f'
+# janela = 'a'
+
+# alarme = (porta == 'a') or (janela == 'a')
+# msg = 'Alarme disparado? ' + str(alarme)
+# print(msg)
+
+tem_dinheiro = True
+tem_dinheiro = not tem_dinheiro
+msg = 'Tem dinheiro? ' + str(tem_dinheiro)
+print(msg)
